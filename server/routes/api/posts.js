c1432006ac3fd85a8fd7d3b98b26e24e37983799
Nasirul Islam <nasirul.siat@outlook.com>
2020-03-12 01:23:47 +0600

    done with the front end and back end

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -14,8 +14,8 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     const posts = await loadPostsCollection();
     await posts.insertOne({
-        test: req.body.text,
-        creadtedAt: new Date()
+        text: req.body.text,
+        createdAt: new Date()
     });
     res.status(201).send();
 });
@@ -31,7 +31,8 @@ router.delete('/:id', async (req, res) => {
 // function to connect post 
 async function loadPostsCollection() {
     const client = await mongodb.MongoClient.connect('mongodb+srv://abcd:siat123456@cluster0-pufwr.mongodb.net/test?retryWrites=true&w=majority', {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
 
     return client.db('vue_express').collection('posts');
