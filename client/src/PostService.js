c50d91ffbda27a63e49e2a4d419636706d57d586
Nasirul Islam <nasirul.siat@outlook.com>
2020-06-25 11:41:01 +0600

    added the front end client side app

---
+++ client/src/PostService.js
@@ -0,0 +1,38 @@
+// Handle all the request 
+import axios from 'axios';
+
+const url = 'api/posts/';
+
+class PostService {
+    // Get Post, returns a promise 
+    static getPosts() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            } catch(err) {
+                reject(err);
+            }
+        })
+    }
+
+    // Create Post 
+    static insertPost(text) {
+        return axios.post(url, {
+            text
+        });
+    }
+
+    // Delete Post 
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`);
+    }
+}
+
+export default PostService;
